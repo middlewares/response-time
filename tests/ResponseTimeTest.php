eb72822d0607d445377dce6f89b60c852ada001a
oscarotero <oom@oscarotero.com>
2016-12-06 18:14:19 +0100

    updated tests

--- tests/ResponseTimeTest.php
+++ tests/ResponseTimeTest.php
@@ -4,17 +4,14 @@ namespace Middlewares\Tests;
 
 use Middlewares\ResponseTime;
 use Middlewares\Utils\Dispatcher;
-use Middlewares\Utils\Factory;
 
 class ResponseTimeTest extends \PHPUnit_Framework_TestCase
 {
     public function testResponseTime()
     {
-        $request = Factory::createServerRequest();
-
-        $response = (new Dispatcher([
+        $response = Dispatcher::run([
             new ResponseTime(),
-        ]))->dispatch($request);
+        ]);
 
         $this->assertInstanceOf('Psr\\Http\\Message\\ResponseInterface', $response);
         $this->assertRegexp('/^\d{1,4}\.\d{3}ms$/', $response->getHeaderLine('X-Response-Time'));
