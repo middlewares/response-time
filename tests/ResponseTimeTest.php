b1e5f8aa3bbe1cbc01c2240793fd20d2fe3e8c8d
oscarotero <oom@oscarotero.com>
2016-12-06 17:00:47 +0100

    updated tests

--- tests/ResponseTimeTest.php
+++ tests/ResponseTimeTest.php
@@ -4,20 +4,17 @@ namespace Middlewares\Tests;
 
 use Middlewares\ResponseTime;
 use Middlewares\Utils\Dispatcher;
-use Middlewares\Utils\CallableMiddleware;
-use Zend\Diactoros\ServerRequest;
-use Zend\Diactoros\Response;
+use Middlewares\Utils\Factory;
 
 class ResponseTimeTest extends \PHPUnit_Framework_TestCase
 {
     public function testResponseTime()
     {
+        $request = Factory::createServerRequest();
+
         $response = (new Dispatcher([
             new ResponseTime(),
-            new CallableMiddleware(function () {
-                return new Response();
-            }),
-        ]))->dispatch(new ServerRequest());
+        ]))->dispatch($request);
 
         $this->assertInstanceOf('Psr\\Http\\Message\\ResponseInterface', $response);
         $this->assertRegexp('/^\d{1,4}\.\d{3}ms$/', $response->getHeaderLine('X-Response-Time'));
